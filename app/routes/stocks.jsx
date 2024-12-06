7bb5268203816b7b941f2e72171d62a9bb658493
christopher.berglund <christopher.berglund@se360.se>
2024-12-06 15:55:04 +0100

    mmmm

--- app/routes/stocks.jsx
+++ app/routes/stocks.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from 'react';
-
-export default function Stocks() {
-    const [stocks, setStocks] = useState([]);
-
-    useEffect(() => {
-        
-        fetch('https://testapi-6jwh.onrender.com/stock')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Fetched stock data:', data);
-                setStocks(data);
-            })
-            .catch((error) => console.error('Error fetching stock data:', error));
-    }, []);  
-
-    return (
-        <div>
-            <h1>Stocks</h1>
-            <ul>
-                {stocks.map((stock) => (
-                    <li key={stock.id}>
-                        {stock.name} - ${stock.price}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+// import { useEffect, useState } from 'react';
+//
+// export default function Stocks() {
+//     const [stocks, setStocks] = useState([]);
+//
+//     useEffect(() => {
+//        
+//         fetch('https://testapi-6jwh.onrender.com/stock')
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 console.log('Fetched stock data:', data);
+//                 setStocks(data);
+//             })
+//             .catch((error) => console.error('Error fetching stock data:', error));
+//     }, []);  
+//
+//     return (    
+//         <div>
+//             <h1>Stocks</h1>
+//             <ul>
+//                 {stocks.map((stock) => (
+//                     <li key={stock.id}>
+//                         {stock.name} - ${stock.price}
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
